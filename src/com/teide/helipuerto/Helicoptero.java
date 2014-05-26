2b222be142d8cc466404861c3fc468297e4edf19
D3mps3y <dam.dempsey.c@gmail.com>
2014-05-26 09:27:08 +0200

    Subida Helipuerto

---
+++ src/com/teide/helipuerto/Helicoptero.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.teide.helipuerto;
+
+/**
+ *
+ * @author DAM
+ */
+public class Helicoptero implements Volador{
+
+    @Override
+    public String despegar() {
+return "El Helicopterio ha despegado ";
+    }
+
+    @Override
+     public String aterrizar(int metros) {
+     return "El Helicopterio ha aterrizado con un coste de " + (0.85*metros)+ "  €";
+    }
+    
+    
+}
